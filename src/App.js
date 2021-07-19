219417789f1989b85426e0d4336bfb390fc76ecd
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-07-19 18:49:30 -0400

    in App.js add header with h1 for title, in src folder create components folder

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <header>
+        <h1>Michael's Todos</h1>
+      </header>
     </div>
   );
 }
