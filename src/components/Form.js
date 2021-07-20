28c11f2d9c1efab939aef09fa8b128e970b0a502
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-07-20 18:14:39 -0400

    in App.js import Form component, render component below header

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Form = () => {
   return(
     <form>
-      <input type="text" class="todo-input">
+      <input type="text" class="todo-input" />
       <button class="todo-button" type="submit">
         <i class="fas fa-plus-square"></i>
       </button>
