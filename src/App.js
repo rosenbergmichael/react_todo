d126b20a2ba76ceb66b5fbac6f040a282b24221a
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-07-21 08:55:32 -0400

    in App.js import useState, set inputText state initialized to empy string

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Form from './components/Form';
 import TodoList from './components/TodoList';
 
 function App() {
+  const [inputText, setInputText] = useState('');
   return (
     <div className="App">
       <header>
